e7b786d1a4c175bd8b2f2e1c83d3c1c5cdd82b2b
edwin66627 <edwinqs27@gmail.com>
2021-09-01 21:15:57 -0500

    Added middleware to reuse sorting, filtering & pagination

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,9 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -20,7 +23,7 @@ router.use('/:id/courses', courseRouter);
 
 router
   .route('/')
-  .get(getBootcamps)
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
   .post(createBootcamp);
 
 router
